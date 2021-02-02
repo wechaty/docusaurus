b73f8dfff041a269dac5317f93b7419436b02296
Huan <zixia@zixia.net>
2021-02-02 20:56:52 +0800

    test with echo

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,13 +4,17 @@
 #
 
 # GitHub Actions is using `shell: /bin/bash -e {0}`
-set +e
+# set +e
 
 if npm test > /dev/null 2>&1; then
   echo 'NPM Testing passed.'
   exit 0
 fi
 
+echo 1
 2>/dev/null npm test  | grep '^not ok ' || true
+echo 2
 1>/dev/null npm test                    || true
+echo 3
+
 exit 1
