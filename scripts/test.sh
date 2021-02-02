92f9c994a60fc4569e926cdb51fba3e46280c824
Huan <zixia@zixia.net>
2021-02-02 18:51:36 +0800

    set +e for my test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-
+#
 # Huan(202101): Only show unit tests that failed for easier debug
+#
+
+# GitHub Actions is using `shell: /bin/bash -e {0}`
+set +e
 
 if npm test > /dev/null 2>&1; then
   echo 'NPM Testing passed.'
