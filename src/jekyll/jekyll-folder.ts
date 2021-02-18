0ef4f5c13fcd60dcf647eb945fa42c7850c0fe1d
Huan LI (李卓桓) <zixia@zixia.net>
2021-02-18 15:42:20 +0800

    modulize get updated files list function

--- src/jekyll/jekyll-folder.ts
+++ src/jekyll/jekyll-folder.ts
@@ -5,9 +5,9 @@ import { REPO_ROOT } from '../repo-root'
 const root = path.join(REPO_ROOT, 'jekyll')
 
 const JEKYLL_FOLDER = {
-  assets     : path.join(root, 'assets'),
+  assets       : path.join(root, 'assets'),
   contributors : path.join(root, '_contributors'),
-  posts      : path.join(root, '_posts'),
+  posts        : path.join(root, '_posts'),
   root,
 }
 
