42e10fa8f2e9f50ef53485de3f2458d42fbb3282
Huan LI (李卓桓) <zixia@zixia.net>
2021-03-18 11:19:20 +0800

    clean

--- docusaurus/src/components/qrcode.tsx
+++ docusaurus/src/components/qrcode.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { QRCode } from 'react-qr-svg'
 
 export default function QrCodeComponent (props) {
-  const value = decodeURIComponent(props.match.params[0]) || 'https://wechaty.js.org/'
+  const value = decodeURIComponent(props.match.params[0]) || 'https://wechaty.js.org'
 
   return <QRCode
     bgColor="#FFFFFF00"
     fgColor="#44a838"
     level="Q"
-    style={{ width: 256, padding: 10 }}
+    style={{
+      padding : 10,
+      width   : 256,
+    }}
     value={value}
   />
 }
