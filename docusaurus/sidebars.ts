b51e5168b351cbbc243062a5bb1949ef953dc152
nibble0101 <mj.mawa@yahoo.com>
2021-05-20 16:53:37 +0300

    Add Wechaty svg and What can you do with Wechaty to Introduction sidebar

--- docusaurus/sidebars.ts
+++ docusaurus/sidebars.ts
@@ -193,6 +193,7 @@ const introduction = {
   items: [
     'overview',
     'wechaty',
+    'what-can-you-do-with-wechaty',
     subMenu(showcases),
     subMenu(caseStudy),
     subMenu(community),
