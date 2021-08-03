b41d82cad8c06b4ba5dfcbc2ab34250e42c699c1
nibble0101 <mj.mawa@yahoo.com>
2021-08-03 16:11:22 +0300

    Merge original and new content on what is wechaty

--- docusaurus/sidebars.ts
+++ docusaurus/sidebars.ts
@@ -211,7 +211,7 @@ const introduction = {
   label: 'Introduction',
   items: [
     'overview',
-    'what-is-wechaty',
+    'wechaty',
     subMenu(showcases),
     subMenu(caseStudy),
     subMenu(community),
