7db2d4239c89cc2db27e39d70525cd9c103eec44
Arnab <encodearnab@gmail.com>
2021-09-10 22:07:36 +0530

    Updated Suggested Changes to Lets Get Started Section

--- docusaurus/src/pages/lets-get-started/lets-get-started-section.tsx
+++ docusaurus/src/pages/lets-get-started/lets-get-started-section.tsx
@@ -1,64 +1,67 @@
 import React from "react";
-import styles from "./lets-get-started-section.module.css";
+import "./lets-get-started-section.css";
 
-const LetsDiveIn = () => {
+const LetsGetStarted = () => {
   return (
-    <div className={styles.body}>
-      <div className={styles.developersPortalHeadlineWrapper}>
-        <div className={styles.developersPortalRectangle} />
-        <p className={styles.developersPortalHeadline}> Let's Get Started </p>
-      </div>
-      <div className={styles.developersPortalSectionWrapper}>
-        <div className={styles.tutorialsWrapper}>
-          <div className={styles.contentWrapperOne}>
+    <main id="body">
+      <section>
+        <div id="developersPortalHeadlineWrapper">
+          <div id="developersPortalRectangle" />
+          <p id="developersPortalHeadline"> Let's Get Started </p>
+        </div>
+        <div id="developersPortalSectionWrapper">
+          <div id="tutorialsWrapper">
+            <div id="contentWrapperOne">
+              <img
+                alt="Web Tutorials Image"
+                id="tutorialsImage"
+                src="https://user-images.githubusercontent.com/31739964/132522682-ca83fa36-8fbc-43c6-b2e2-e10946568e14.png"
+              />
+              <p id="tutorialsHeadline">Tutorials</p>
+              <p id="developersPortalContent">
+                If you are a total beginner to Wechaty or Chatbot application
+                development in general, we recommend you start from the
+                tutorials.
+              </p>
+              <div id="takeMeThereLinkButtonOne">
+                <a id="takeMeThereLink">Take me there</a>
+              </div>
+            </div>
+          </div>
+          <div id="contentWrapperTwo">
             <img
-              alt="Web Tutorials Image"
-              className={styles.tutorialsImage}
-              src="https://user-images.githubusercontent.com/31739964/132522682-ca83fa36-8fbc-43c6-b2e2-e10946568e14.png"
+              alt="Question Mark on a piece of Code"
+              id="howToGuidesImage"
+              src="https://user-images.githubusercontent.com/31739964/132522672-59232488-7912-4898-bf3e-2cf5b1a205f0.png"
             />
-            <p className={styles.tutorialsHeadline}>Tutorials</p>
-            <p className={styles.developersPortalContent}>
-              If you are a total beginner to Wechaty or Chatbot application
-              development in general, we recommend you start from the tutorials.
+            <p id="howToGuidesHeadline">How-to Guides</p>
+            <p id="developersPortalContent">
+              They are more advanced than tutorials and pre requisite knowledge
+              is required of how Wechaty works.
             </p>
-            <div className={styles.takeMeThereLinkButtonOne}>
-              <a className={styles.takeMeThereLink}>Take me there</a>
+            <div id="takeMeThereLinkButtonTwo">
+              <a id="takeMeThereLink">Take me there</a>
+            </div>
+          </div>
+          <div id="contentWrapperThree">
+            <img
+              src="https://user-images.githubusercontent.com/31739964/132522680-7f2b20b3-2b91-4a0b-8f02-89ddaae651b0.png"
+              id="referencesImage"
+              alt="Reference Book Image"
+            />
+            <p id="refrencesHeadline">Refrences</p>
+            <p id="developersPortalContent">
+              References contain technical reference for APIs and other aspects
+              of Wechaty&#039;s machinery.
+            </p>
+            <div id="takeMeThereLinkButtonThree">
+              <a id="takeMeThereLink">Take me there</a>
             </div>
           </div>
         </div>
-        <div className={styles.contentWrapperTwo}>
-          <img
-            alt="Question Mark on a piece of Code"
-            className={styles.howToGuidesImage}
-            src="https://user-images.githubusercontent.com/31739964/132522672-59232488-7912-4898-bf3e-2cf5b1a205f0.png"
-          />
-          <p className={styles.howToGuidesHeadline}>How-to Guides</p>
-          <p className={styles.developersPortalContent}>
-            They are more advanced than tutorials and pre requisite knowledge is
-            required of how Wechaty works.
-          </p>
-          <div className={styles.takeMeThereLinkButtonTwo}>
-            <a className={styles.takeMeThereLink}>Take me there</a>
-          </div>
-        </div>
-        <div className={styles.contentWrapperThree}>
-          <img
-            src="https://user-images.githubusercontent.com/31739964/132522680-7f2b20b3-2b91-4a0b-8f02-89ddaae651b0.png"
-            className={styles.referencesImage}
-            alt="Reference Book Image"
-          />
-          <p className={styles.refrencesHeadline}>Refrences</p>
-          <p className={styles.developersPortalContent}>
-            References contain technical reference for APIs and other aspects of
-            Wechaty&#039;s machinery.
-          </p>
-          <div className={styles.takeMeThereLinkButtonThree}>
-            <a className={styles.takeMeThereLink}>Take me there</a>
-          </div>
-        </div>
-      </div>
-    </div>
+      </section>
+    </main>
   );
 };
 
-export default LetsDiveIn;
+export default LetsGetStarted;
