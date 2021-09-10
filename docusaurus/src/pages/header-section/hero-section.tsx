31c700be462fa4a34de6a70c336177052215c98c
Arnab <encodearnab@gmail.com>
2021-09-10 21:16:26 +0530

    Updated Suggested Changes

--- docusaurus/src/pages/header-section/hero-section.tsx
+++ docusaurus/src/pages/header-section/hero-section.tsx
@@ -1,37 +1,37 @@
 import React from "react";
-import styles from "./hero-section.module.css";
+import "./hero-section.css";
 
 const HeroSection = () => {
   return (
-    <div className={styles.body}>
-      <div className={styles.heroSection}>
-        <p className={styles.heroTagLine}>
+    <main id="body">
+      <section id="heroSection">
+        <headline id="heroTagLine">
           In just a few lines of code, you can have a fully featured
-        </p>
-        <div className={styles.heroTagLineWrapper}>
-          <p className={styles.heroTagLineEmphasis1}>
+        </headline>
+        <div id="heroTagLineWrapper">
+          <p id="heroTagLineEmphasis1">
             <a target="_blank"> Chatbot </a> &nbsp;!
             <a target="_blank"> &nbsp;! </a>
             &nbsp;!
           </p>
         </div>
-        <p className={styles.heroSlogan}>
+        <p id="heroSlogan">
           {" "}
           &#123; Code less works more, that's wechaty ! &#125;{" "}
         </p>
         <img
           alt="Chatbot"
-          className={styles.heroChatbotImage}
+          id="heroChatbotImage"
           src="https://user-images.githubusercontent.com/31739964/132517171-4eb33791-30ea-4f17-91ee-e3b170e8ef19.png"
         />
-        <button className={styles.letsDiveInButton}> Let’s Dive In </button>
+        <button id="letsDiveInButton"> Let’s Dive In </button>
         <img
           alt="Chatbot"
-          className={styles.heroImageMobile}
+          id="heroImageMobile"
           src="https://user-images.githubusercontent.com/31739964/132517179-5802f6c3-1209-41d7-b993-3f2b197adc2d.png"
         />
-      </div>
-    </div>
+      </section>
+    </main>
   );
 };
 
