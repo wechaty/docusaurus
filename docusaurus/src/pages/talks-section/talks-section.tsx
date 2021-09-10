57decfcfe18b8c093c6369bfaa042cb7dccef0b2
Arnab <encodearnab@gmail.com>
2021-09-10 22:25:03 +0530

    Updated Suggested Changes for Talks Section

--- docusaurus/src/pages/talks-section/talks-section.tsx
+++ docusaurus/src/pages/talks-section/talks-section.tsx
@@ -1,21 +1,21 @@
 import React from "react";
-import styles from "./talks-section.module.css";
+import "./talks-section.css";
 
 const WechatyLandingPage = () => {
   return (
-    <div className={styles.body}>
-      <div className={styles.talksSectionWrapper}>
-        <div className={styles.talksRectangle} />
-        <p className={styles.talksHeadline}> Talks </p>
-        <div className={styles.talksBothWrapper}>
-          <div className={styles.talksWrapper}>
-            <div className={styles.talksUpperWrapper}>
+    <main id="body">
+      <section id="talksSectionWrapper">
+        <div id="talksRectangle" />
+        <p id="talksHeadline"> Talks </p>
+        <div id="talksBothWrapper">
+          <div id="talksWrapper">
+            <div id="talksUpperWrapper">
               <img
                 alt="Award Image"
-                className={styles.talksImage}
+                id="talksImage"
                 src="https://static.overlay-tech.com/assets/b52ac311-3f89-49c2-8fb4-4a6bb700f86f.png"
               />
-              <p className={styles.talksDetail}>
+              <p id="talksDetail">
                 Speaker -
                 <br />
                 Huan Li
@@ -26,27 +26,27 @@ const WechatyLandingPage = () => {
                 Azure SHOW
               </p>
             </div>
-            <div className={styles.talksContentWrapper}>
-              <p className={styles.talksContentHeadline}>
+            <div id="talksContentWrapper">
+              <p id="talksContentHeadline">
                 Wechaty 2016 - 2020 : Four Years of Open Source
               </p>
-              <p className={styles.talksContent}>
+              <p id="talksContent">
                 Welcome to the open source story of AzureShow, the first issue
                 guest Huan Li, the co-author of the first Conversational User
                 Interface best-selling book "Chatbot from 0 to 1" in Chinese,
                 the author of Wechaty.
               </p>
-              <a className={styles.readMoreLink}>Read More</a>
+              <a id="readMoreLink">Read More</a>
             </div>
           </div>
-          <div className={styles.talksWrapper}>
-            <div className={styles.talksUpperWrapper}>
+          <div id="talksWrapper">
+            <div id="talksUpperWrapper">
               <img
                 alt="Award Image Two"
-                className={styles.talksImage}
+                id="talksImage"
                 src="https://static.overlay-tech.com/assets/d1a2bfce-b571-4607-a668-4a271328f987.png"
               />
-              <p className={styles.talksDetail}>
+              <p id="talksDetail">
                 Speaker -
                 <br />
                 Huan Li
@@ -57,11 +57,11 @@ const WechatyLandingPage = () => {
                 Node Party
               </p>
             </div>
-            <div className={styles.talksContentWrapper}>
-              <p className={styles.talksContentHeadline}>
+            <div id="talksContentWrapper">
+              <p id="talksContentHeadline">
                 Wechaty 101: from v0.0 to v0.7
               </p>
-              <p className={styles.talksContent}>
+              <p id="talksContent">
                 This talk was presented at Node Party Beijing #18 Dec 2016, all
                 about ChatBots.
                 <br />
@@ -73,12 +73,12 @@ const WechatyLandingPage = () => {
                 <br />
                 what’s in it for me?
               </p>
-              <a className={styles.readMoreLink}>Read More</a>
+              <a id="readMoreLink">Read More</a>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </section>
+    </main>
   );
 };
 
