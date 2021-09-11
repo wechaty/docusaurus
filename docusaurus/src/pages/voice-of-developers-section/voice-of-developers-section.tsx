000f36a1f9c7331f88da57a3ca66d2849525fc32
sajen sarvajith <sajenjeshan1222@gmail.com>
2021-09-11 15:23:06 +0530

    done the suggested changes

--- docusaurus/src/pages/voice-of-developers-section/voice-of-developers-section.tsx
+++ docusaurus/src/pages/voice-of-developers-section/voice-of-developers-section.tsx
@@ -1,40 +1,38 @@
 import React from "react";
-import styles from "./voice-of-developers-section.module.css";
+import "./voice-of-developers-section.css";
 
 const voiceOfDevelopers = () => {
 
     return (
-        <div className={styles.body}>
-        <div className={styles.voiceOfDevelopersWrapper}>
-        <div className={styles.voiceOfDevelopersRectangle} />
-        <p className={styles.voiceOfDevelopersHeadline}>Voice of Developers</p>
-        <div className={styles.voiceOfDevelopersAllWrapper}>
-          <div className={styles.voiceOfDevelopersOne}>
-            <img className={styles.voiceOfDevelopersImageOne} />
-            <p className={styles.voiceOfDevelopersText}>
-              "This is an example text, voice of developers comes here"
-            </p>
-          </div>
-          <div className={styles.voiceOfDevelopersTwo}>
-            <img className={styles.voiceOfDevelopersImageTwo} />
-            <p className={styles.voiceOfDevelopersText}>
-              "This is an example text, voice of developers comes here"
-            </p>
-          </div>
-          <div className={styles.voiceOfDevelopersThree}>
-            <img className={styles.voiceOfDevelopersImageThree} />
-            <p className={styles.voiceOfDevelopersText}>
-              "This is an example text, voice of developers comes here"
-            </p>
-          </div>
+      <main id="body">
+      <section id="voiceOfDevelopersWrapper">
+      <div id="voiceOfDevelopersRectangle" />
+      <headline id="voiceOfDevelopersHeadline">Voice of Developers</headline>
+      <div id="voiceOfDevelopersAllWrapper">
+        <div id="voiceOfDevelopersOne">
+          <img id="voiceOfDevelopersImageOne" />
+          <p id="voiceOfDevelopersText">
+            "This is an example text, voice of developers comes here"
+          </p>
         </div>
-        <div className={styles.ourContributorsButtonWrapper}>
-          <button className={styles.ourContributorsButton}>
-            Meet our Contributors
-          </button>
+        <div id="voiceOfDevelopersTwo">
+          <img id="voiceOfDevelopersImageTwo" />
+          <p id="voiceOfDevelopersText">
+            "This is an example text, voice of developers comes here"
+          </p>
+        </div>
+        <div id="voiceOfDevelopersThree">
+          <img id="voiceOfDevelopersImageThree" />
+          <p id="voiceOfDevelopersText">
+            "This is an example text, voice of developers comes here"
+          </p>
         </div>
       </div>
+      <div id="ourContributorsButtonWrapper">
+        <button id="ourContributorsButton">Meet our Contributors</button>
       </div>
+    </section>
+    </main>
 
     );
 
