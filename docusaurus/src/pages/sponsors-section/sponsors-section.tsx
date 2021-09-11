f61b17b3e4a1941ecbb3cc5253edf6a71b8f7edb
sajen sarvajith <sajenjeshan1222@gmail.com>
2021-09-11 15:37:08 +0530

    finished the changes

--- docusaurus/src/pages/sponsors-section/sponsors-section.tsx
+++ docusaurus/src/pages/sponsors-section/sponsors-section.tsx
@@ -1,56 +1,72 @@
 import React from "react";
-import styles from "./sponsors-section.module.css";
+import "./sponsors-section.css";
+import sponsorsOneImage from "./sponsorOne.png";
+import sponsorsTwoImage from "./sponsorTwo.png";
+import sponsorsThreeImage from "./sponsorThree.png";
 
 const sponsors = () => {
     return (
       <main id="body">
 <section>
-        <div id="honorsAndAwardsHeadlineWrapper">
-          <div id="honorsAndAwardsRectangle" />
-          <headline id="honorsAndAwardsHeadline">Honors &amp; Awards</headline>
-        </div>
-        <div id="honorsAndAwardsSection">
-          <div id="honorsAndAwardsFirstWrapper">
-            <headline id="honorsAndAwardsFirstHeadline">
-              Wechaty won the award from the top domestic open source
-              organization
-            </headline>
-            <div id="honorsAndAwardsFirstContentWrapper">
-              <p id="honorsAndAwardsFirstContent">
-                On December 30, Wechaty won important honours at the &#34;Tenth
-                China Cloud Computing Standards and Application Conference&#34;
-                organized by the China Open Source Cloud Alliance.
-              </p>
-              <a id="learnMoreButtonOne">Learn more</a>
+        <div id="sponsorsRectangle" />
+        <headline id="sponsorsHeadline">Our Sponsors</headline>
+        <div id="sponsorsSectionWrapper">
+          <div id="sponsorsFirstSection">
+            <div id="sponsorsOne">
+              <img
+                alt="Sponsor Image One"
+                src={sponsorsThreeImage}
+                id="sponsorsOneImage"
+              ></img>
             </div>
-            <img
-              alt="Award One Image"
-              id="awardImageOne"
-              src="https://static.overlay-tech.com/assets/6e34fc8d-8492-4328-b867-cb16a3507b53.png"
-            />
+            <div id="sponsorsThree">
+              <img
+                alt="Sponsor Image Three"
+                src="https://static.overlay-tech.com/assets/cde3df57-5f07-4e68-b220-4d2c817ee75a.svg"
+                id="sponsorsThreeImage"
+              ></img>
+            </div>
+            <div id="sponsorsFive">
+              <img
+                alt="Sponsor Image Five"
+                src={sponsorsTwoImage}
+                id="sponsorsFiveImage"
+              ></img>
+            </div>
+            <div id="sponsorsSeven">
+              <img
+                alt="Sponsor Image Seven"
+                src="https://static.overlay-tech.com/assets/401b43af-c035-48d7-a739-798731ed4e12.png"
+                id="sponsorsSevenImage"
+              ></img>
+            </div>
           </div>
-          <div id="honorsAndAwardsSecondWrapper">
-            <headline id="honorsAndAwardsSecondHeadline">
-              Wechaty, was selected as &#34;33 Chinese Open Source Pioneers&#34;
-            </headline>
-            <div id="honorsAndAwardsSecondContentWrapper">
-              <p id="honorsAndAwardsSecondContent">
-                Wechaty creator, BOT5 Club Chair, Tencent Chatbot TVP, and
-                PreAngel Partner Li Zhuohuan was named to the
-                SegmentFault&#039;s 2nd annual list of 33 Chinese Open Source
-                Pioneers.&#34;
-              </p>
-              <a id="learnMoreButtonTwo">Learn more</a>
+          <div id="sponsorsSecondSection">
+            <div id="sponsorsTwo">
+              <img
+                alt="Sponsor Image Two"
+                src="https://static.overlay-tech.com/assets/222afd35-a8f3-4c41-82ad-8cd10366b362.svg"
+                id="sponsorsTwoImage"
+              ></img>
             </div>
-            <img
-              alt="Award Two Image"
-              id="awardImageTwo"
-              src="https://static.overlay-tech.com/assets/b3ff4dd1-cc78-4275-b428-fa17968ee60d.png"
-            />
+            <div id="sponsorsFour">
+              <img
+                alt="Sponsor Image Four"
+                src={sponsorsOneImage}
+                id="sponsorsFourImage"
+              ></img>
+            </div>
+            <div id="sponsorsSix">
+              <img
+                alt="Sponsor Image Six"
+                src="https://static.overlay-tech.com/assets/d391db82-7c1a-4c81-ab80-c2a194eb5020.png"
+                id="sponsorsSixImage"
+              ></img>
+            </div>
           </div>
         </div>
       </section>
-      </main>
+    </main>
     );
 };
 export default sponsors;
