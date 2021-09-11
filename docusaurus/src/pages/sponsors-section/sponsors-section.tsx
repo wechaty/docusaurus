2656388769331bebcf363e6b27fc68be4b730ff4
sajen sarvajith <sajenjeshan1222@gmail.com>
2021-09-11 14:35:21 +0530

    finished the suggested work

--- docusaurus/src/pages/sponsors-section/sponsors-section.tsx
+++ docusaurus/src/pages/sponsors-section/sponsors-section.tsx
@@ -3,58 +3,54 @@ import styles from "./sponsors-section.module.css";
 
 const sponsors = () => {
     return (
-<div className={styles.body}>
-<div className={styles.sponsorsRectangle} />
-<p className={styles.sponsorsHeadline}>Our Sponsors</p>
-<div className={styles.sponsorsSectionWrapper}>
-  <div className={styles.sponsorsFirstSection}>
-    <div className={styles.sponsorsOne}>
-      <img
-        src="https://user-images.githubusercontent.com/31739964/132560333-6daecea2-b0cb-44ea-bc83-c857fc6eb667.png"
-        className={styles.sponsorsOneImage}
-      ></img>
-    </div>
-    <div className={styles.sponsorsThree}>
-      <img
-        src="https://static.overlay-tech.com/assets/cde3df57-5f07-4e68-b220-4d2c817ee75a.svg"
-        className={styles.sponsorsThreeImage}
-      ></img>
-    </div>
-    <div className={styles.sponsorsFive}>
-      <img
-        src="https://user-images.githubusercontent.com/31739964/132560519-ac0eb677-fc8b-4884-8de7-ce7c446bbeb1.png"
-        className={styles.sponsorsFiveImage}
-      ></img>
-    </div>
-    <div className={styles.sponsorsSeven}>
-      <img
-        src="https://static.overlay-tech.com/assets/401b43af-c035-48d7-a739-798731ed4e12.png"
-        className={styles.sponsorsSevenImage}
-      ></img>
-    </div>
-  </div>
-  <div className={styles.sponsorsSecondSection}>
-    <div className={styles.sponsorsTwo}>
-      <img
-        src="https://static.overlay-tech.com/assets/222afd35-a8f3-4c41-82ad-8cd10366b362.svg"
-        className={styles.sponsorsTwoImage}
-      ></img>
-    </div>
-    <div className={styles.sponsorsFour}>
-      <img
-        src="https://user-images.githubusercontent.com/31739964/132561006-587c1d50-e5eb-4c1d-980d-9887310d8a18.png"
-        className={styles.sponsorsFourImage}
-      ></img>
-    </div>
-    <div className={styles.sponsorsSix}>
-      <img
-        src="https://static.overlay-tech.com/assets/d391db82-7c1a-4c81-ab80-c2a194eb5020.png"
-        className={styles.sponsorsSixImage}
-      ></img>
-    </div>
-  </div>
-</div>
-</div>
+      <main id="body">
+<section>
+        <div id="honorsAndAwardsHeadlineWrapper">
+          <div id="honorsAndAwardsRectangle" />
+          <headline id="honorsAndAwardsHeadline">Honors &amp; Awards</headline>
+        </div>
+        <div id="honorsAndAwardsSection">
+          <div id="honorsAndAwardsFirstWrapper">
+            <headline id="honorsAndAwardsFirstHeadline">
+              Wechaty won the award from the top domestic open source
+              organization
+            </headline>
+            <div id="honorsAndAwardsFirstContentWrapper">
+              <p id="honorsAndAwardsFirstContent">
+                On December 30, Wechaty won important honours at the &#34;Tenth
+                China Cloud Computing Standards and Application Conference&#34;
+                organized by the China Open Source Cloud Alliance.
+              </p>
+              <a id="learnMoreButtonOne">Learn more</a>
+            </div>
+            <img
+              alt="Award One Image"
+              id="awardImageOne"
+              src="https://static.overlay-tech.com/assets/6e34fc8d-8492-4328-b867-cb16a3507b53.png"
+            />
+          </div>
+          <div id="honorsAndAwardsSecondWrapper">
+            <headline id="honorsAndAwardsSecondHeadline">
+              Wechaty, was selected as &#34;33 Chinese Open Source Pioneers&#34;
+            </headline>
+            <div id="honorsAndAwardsSecondContentWrapper">
+              <p id="honorsAndAwardsSecondContent">
+                Wechaty creator, BOT5 Club Chair, Tencent Chatbot TVP, and
+                PreAngel Partner Li Zhuohuan was named to the
+                SegmentFault&#039;s 2nd annual list of 33 Chinese Open Source
+                Pioneers.&#34;
+              </p>
+              <a id="learnMoreButtonTwo">Learn more</a>
+            </div>
+            <img
+              alt="Award Two Image"
+              id="awardImageTwo"
+              src="https://static.overlay-tech.com/assets/b3ff4dd1-cc78-4275-b428-fa17968ee60d.png"
+            />
+          </div>
+        </div>
+      </section>
+      </main>
     );
 };
 export default sponsors;
