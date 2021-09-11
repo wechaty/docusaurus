f9786baf162315e1ede1288c0a3f175d5125f979
sajen sarvajith <sajenjeshan1222@gmail.com>
2021-09-11 14:48:21 +0530

    Update honors-and-awards-section.tsx

--- docusaurus/src/pages/honors-and-awards-section/honors-and-awards-section.tsx
+++ docusaurus/src/pages/honors-and-awards-section/honors-and-awards-section.tsx
@@ -1,54 +1,57 @@
 import React from "react";
-import styles from "./honors-and-awards-section.module.css";
+import "./honors-and-awards-section.css";
 
 const honorsAndAwards = () => {
     
     return(
-        <div className={styles.body}>
-        <div className={styles.honorsAndAwardsHeadlineWrapper}>
-        <div className={styles.honorsAndAwardsRectangle} />
-        <p className={styles.honorsAndAwardsHeadline}>Honors &amp; Awards</p>
-      </div>
-      <div className={styles.honorsAndAwardsSection}>
-        <div className={styles.honorsAndAwardsFirstWrapper}>
-          <p className={styles.honorsAndAwardsFirstHeadline}>
-            Wechaty won the award from the top domestic open source organization
-          </p>
-          <div className={styles.honorsAndAwardsFirstContentWrapper}>
-            <p className={styles.honorsAndAwardsFirstContent}>
-              On December 30, Wechaty won important honours at the &#34;Tenth
-              China Cloud Computing Standards and Application Conference&#34;
-              organized by the China Open Source Cloud Alliance.
-            </p>
-            <a className={styles.learnMoreButtonOne}>Learn more</a>
-          </div>
-          <img
-            alt="Award One Image"
-            className={styles.awardImageOne}
-            src="https://static.overlay-tech.com/assets/6e34fc8d-8492-4328-b867-cb16a3507b53.png"
-          />
-        </div>
-        <div className={styles.honorsAndAwardsSecondWrapper}>
-          <p className={styles.honorsAndAwardsSecondHeadline}>
-            Wechaty, was selected as &#34;33 Chinese Open Source Pioneers&#34;
-          </p>
-          <div className={styles.honorsAndAwardsSecondContentWrapper}>
-            <p className={styles.honorsAndAwardsSecondContent}>
-              Wechaty creator, BOT5 Club Chair, Tencent Chatbot TVP, and
-              PreAngel Partner Li Zhuohuan was named to the SegmentFault&#039;s
-              2nd annual list of 33 Chinese Open Source Pioneers.&#34;
-            </p>
-            <a className={styles.learnMoreButtonTwo}>Learn more</a>
-          </div>
-          <img
-            alt="Award Two Image"
-            className={styles.awardImageTwo}
-            src="https://static.overlay-tech.com/assets/b3ff4dd1-cc78-4275-b428-fa17968ee60d.png"
-          />
-        </div>
-      </div>
-      </div>
-
+      <main id="body">
+      <section>
+              <div id="honorsAndAwardsHeadlineWrapper">
+                <div id="honorsAndAwardsRectangle" />
+                <headline id="honorsAndAwardsHeadline">Honors &amp; Awards</headline>
+              </div>
+              <div id="honorsAndAwardsSection">
+                <div id="honorsAndAwardsFirstWrapper">
+                  <headline id="honorsAndAwardsFirstHeadline">
+                    Wechaty won the award from the top domestic open source
+                    organization
+                  </headline>
+                  <div id="honorsAndAwardsFirstContentWrapper">
+                    <p id="honorsAndAwardsFirstContent">
+                      On December 30, Wechaty won important honours at the &#34;Tenth
+                      China Cloud Computing Standards and Application Conference&#34;
+                      organized by the China Open Source Cloud Alliance.
+                    </p>
+                    <a id="learnMoreButtonOne">Learn more</a>
+                  </div>
+                  <img
+                    alt="Award One Image"
+                    id="awardImageOne"
+                    src="https://static.overlay-tech.com/assets/6e34fc8d-8492-4328-b867-cb16a3507b53.png"
+                  />
+                </div>
+                <div id="honorsAndAwardsSecondWrapper">
+                  <headline id="honorsAndAwardsSecondHeadline">
+                    Wechaty, was selected as &#34;33 Chinese Open Source Pioneers&#34;
+                  </headline>
+                  <div id="honorsAndAwardsSecondContentWrapper">
+                    <p id="honorsAndAwardsSecondContent">
+                      Wechaty creator, BOT5 Club Chair, Tencent Chatbot TVP, and
+                      PreAngel Partner Li Zhuohuan was named to the
+                      SegmentFault&#039;s 2nd annual list of 33 Chinese Open Source
+                      Pioneers.&#34;
+                    </p>
+                    <a id="learnMoreButtonTwo">Learn more</a>
+                  </div>
+                  <img
+                    alt="Award Two Image"
+                    id="awardImageTwo"
+                    src="https://static.overlay-tech.com/assets/b3ff4dd1-cc78-4275-b428-fa17968ee60d.png"
+                  />
+                </div>
+              </div>
+            </section>
+            </main>
     );
 };
 export default honorsAndAwards;
