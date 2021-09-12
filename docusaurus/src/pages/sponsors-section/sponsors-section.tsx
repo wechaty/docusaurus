2e8588aebe3bb951c4381dd095e9a5cf92c4851e
sajen sarvajith <sajenjeshan1222@gmail.com>
2021-09-12 21:39:03 +0530

    added the images locally

--- docusaurus/src/pages/sponsors-section/sponsors-section.tsx
+++ docusaurus/src/pages/sponsors-section/sponsors-section.tsx
@@ -14,28 +14,28 @@ const sponsors = () => {
             <div id="sponsorsOne">
               <img
                 alt="Sponsor Image One"
-                src="https://user-images.githubusercontent.com/31739964/132944541-57c8cbe0-1b3b-4292-820e-cdcd1b19ea90.png"
+                src="microsoft.webp"
                 id="sponsorsOneImage"
               ></img>
             </div>
             <div id="sponsorsThree">
               <img
                 alt="Sponsor Image Three"
-                src="https://static.overlay-tech.com/assets/cde3df57-5f07-4e68-b220-4d2c817ee75a.svg"
+                src="tencent.webp"
                 id="sponsorsThreeImage"
               ></img>
             </div>
             <div id="sponsorsFive">
               <img
                 alt="Sponsor Image Five"
-                src="https://user-images.githubusercontent.com/31739964/132944564-6fe38a21-6416-4f0b-8199-ce983cdf7b7c.png"
+                src="juzi.webp"
                 id="sponsorsFiveImage"
               ></img>
             </div>
             <div id="sponsorsSeven">
               <img
                 alt="Sponsor Image Seven"
-                src="https://static.overlay-tech.com/assets/401b43af-c035-48d7-a739-798731ed4e12.png"
+                src="gsod.webp"
                 id="sponsorsSevenImage"
               ></img>
             </div>
@@ -44,21 +44,21 @@ const sponsors = () => {
             <div id="sponsorsTwo">
               <img
                 alt="Sponsor Image Two"
-                src="https://static.overlay-tech.com/assets/222afd35-a8f3-4c41-82ad-8cd10366b362.svg"
+                src="tensorflow.webp"
                 id="sponsorsTwoImage"
               ></img>
             </div>
             <div id="sponsorsFour">
               <img
                 alt="Sponsor Image Four"
-                src="https://user-images.githubusercontent.com/31739964/132944612-fdaac727-35e4-4bc2-a468-312617e72460.png"
+                src="iscas.webp"
                 id="sponsorsFourImage"
               ></img>
             </div>
             <div id="sponsorsSix">
               <img
                 alt="Sponsor Image Six"
-                src="https://static.overlay-tech.com/assets/d391db82-7c1a-4c81-ab80-c2a194eb5020.png"
+                src="sujitech.webp"
                 id="sponsorsSixImage"
               ></img>
             </div>
