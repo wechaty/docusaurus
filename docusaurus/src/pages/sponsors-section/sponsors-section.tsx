d6a8ff71b822e543dca4152b8de455aa8cb7c4c5
sajen sarvajith <sajenjeshan1222@gmail.com>
2021-10-15 13:06:52 +0530

    fixed the image issue

--- docusaurus/src/pages/sponsors-section/sponsors-section.tsx
+++ docusaurus/src/pages/sponsors-section/sponsors-section.tsx
@@ -14,28 +14,28 @@ const sponsors = () => {
             <div id="microsoft">
               <img
                 alt="Sponsor Image One"
-                src="microsoft.webp"
+                src="img/microsoft.webp"
                 id="microsoftImage"
               ></img>
             </div>
             <div id="tencent">
               <img
                 alt="Sponsor Image Three"
-                src="tencent.webp"
+                src="img/tencent.webp"
                 id="tencentImage"
               ></img>
             </div>
             <div id="juzi">
               <img
                 alt="Sponsor Image Five"
-                src="juzi.webp"
+                src="img/juzi.webp"
                 id="juziImage"
               ></img>
             </div>
             <div id="gsod">
               <img
                 alt="Sponsor Image Seven"
-                src="gsod.webp"
+                src="img/gsod.webp"
                 id="gsodImage"
               ></img>
             </div>
@@ -44,21 +44,21 @@ const sponsors = () => {
             <div id="tensorflow">
               <img
                 alt="Sponsor Image Two"
-                src="tensorflow.webp"
+                src="img/tensorflow.webp"
                 id="tensorflowImage"
               ></img>
             </div>
             <div id="iscas">
               <img
                 alt="Sponsor Image Four"
-                src="iscas.webp"
+                src="img/iscas.webp"
                 id="iscasImage"
               ></img>
             </div>
             <div id="sujitech">
               <img
                 alt="Sponsor Image Six"
-                src="sujitech.webp"
+                src="img/sujitech.webp"
                 id="sujitechImage"
               ></img>
             </div>
