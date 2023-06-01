381af696ff316a0b6f3b0cc8aca0230c3752c5a8
NickWang <brotherstyx@gmail.com>
2023-06-01 12:02:42 +0800

    fix: 🐛 qrcode plugin

--- docusaurus/src/plugins/qrcode.js
+++ docusaurus/src/plugins/qrcode.js
@@ -1,6 +1,9 @@
 function qrCodePlugin (context, options) {
   return {
     async contentLoaded ({ content, actions }) {
+      if (context.i18n.currentLocale !== context.i18n.defaultLocale) {
+        return // we don't have to create qrcode page for other locales
+      }
       const { addRoute } = actions
       addRoute({
         component: '@site/src/components/qrcode.tsx',
