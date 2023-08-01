7efcdeea44fe9fcc5574042d24ef3a9c8ddaf342
NickWang <brotherstyx@gmail.com>
2023-08-01 15:11:31 +0800

    Feat/baidu statistics (#1526)
    * feat: 🎸 import baidu-statistics
    
    * 0.11.7

---
+++ docusaurus/static/js/baidu-statistics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?eef0e59d192a0783a44dc3744133d15e";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
